4aff17a95df1f9e2e57be3547a5352d94675fa52
Terry D'Arcy <terrykdarcy@gmail.com>
2022-04-24 23:24:50 +0100

    github prokects update

--- src/app/home/projects/projects.component.ts
+++ src/app/home/projects/projects.component.ts
@@ -14,15 +14,38 @@ export class ProjectsComponent implements OnInit {
   constructor(private githubService: GithubService) { }
 
   ngOnInit(): void {
+    this.fetchAndFormatRepos();
+  }
+
+  fetchAndFormatRepos() {
     this.githubLoading = true;
-    this.githubLoading = false;
     this.githubService.getGitHubRepos().subscribe((result) => {
-        this.repos = result;
-        console.log (this.repos);
-        this.repos.map((repo) => this.githubService.getRepoLanguages(repo.name).subscribe((result) => {
-            repo.languages = Object.keys(result);
-        }));
+      this.repos = result;
+      console.log (this.repos);
+      
+      this.repos.map((repo) => {
+        repo.name = this.capitalizeFirstLetter(repo.name);
+        repo.description = this.capitalizeFirstLetter(repo.description);
+        
+        this.githubService.getRepoLanguages(repo.name).subscribe((result) => {
+          repo.languages = Object.keys(result);
+          this.githubLoading = false;
+        })
+      });
+      //sorting repos by updated_at
+      this.repos.sort((x, y) => {
+        const xDate = new Date(y.updated_at);
+        const yDate = new Date(x.updated_at);
+        if(xDate < yDate) return -1;
+        else if(xDate > yDate) return 1;
+          else return 0;
+        })
     });
+  }
+  
+  capitalizeFirstLetter(string: string) {
+    if(!string) return string;
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
   openRepo(repoURL: string) {
