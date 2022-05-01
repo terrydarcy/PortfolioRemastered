0daebcbb4d0661c32cf642da0f3534ad569d307d
Terry D'Arcy <terrykdarcy@gmail.com>
2022-05-01 13:00:20 +0100

    adding social buttons

--- src/app/home/into/into.component.ts
+++ src/app/home/into/into.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faInstagram, faTwitter, faGithub, faYoutube} from '@fortawesome/free-brands-svg-icons';
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+
 @Component({
   selector: 'app-into',
   templateUrl: './into.component.html',
@@ -6,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntoComponent implements OnInit {
 
-
+  faInstagram = faInstagram as IconProp;
+  faTwitter = faTwitter as IconProp;
+  faGithub = faGithub as IconProp;
+  faYoutube = faYoutube as IconProp;
+ 
   constructor() {
   }
 
