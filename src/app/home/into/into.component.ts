4ef60e5ff64b78ee2d4ff3e1aec156c5d9259609
Terry D'Arcy <terrykdarcy@gmail.com>
2022-05-09 02:15:18 +0100

    spotlight update

--- src/app/home/into/into.component.ts
+++ src/app/home/into/into.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faInstagram, faTwitter, faGithub, faYoutube} from '@fortawesome/free-brands-svg-icons';
+import { faInstagram, faTwitter, faGithub, faYoutube,faLinkedin} from '@fortawesome/free-brands-svg-icons';
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 
 @Component({
@@ -13,10 +13,13 @@ export class IntoComponent implements OnInit {
   faTwitter = faTwitter as IconProp;
   faGithub = faGithub as IconProp;
   faYoutube = faYoutube as IconProp;
+  faLinkedin = faLinkedin as IconProp;
 
   instagramURL : string = 'https://www.instagram.com/darcydevelopment/';
   twitterURL : string = 'https://twitter.com/darcydevelop';
   youtubeURL : string = 'https://www.youtube.com/channel/UChBEieVKmbciCLEaeog5u4g';
+  linkedinURL : string = 'https://www.linkedin.com/in/terry-d-arcy-b35383153/';
+  githubURL: string = 'https://www.github.com/terrydarcy';
  
   constructor() {
   }
