39c03c60a7d877a1f04c27236db9a13d47c64512
Terry D'Arcy <terrykdarcy@gmail.com>
2022-12-10 15:22:31 +0000

    added aws certification

--- src/app/home/experience/experience.component.ts
+++ src/app/home/experience/experience.component.ts
@@ -4,15 +4,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.scss']
+  styleUrls: ['./experience.component.scss'],
 })
 export class ExperienceComponent implements OnInit {
-
   // vodafone = vodafone;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  goToURL(path: string) {
+    window.open(path, '_blank');
   }
-
 }
--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,23 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
- @Injectable({
-  providedIn: 'root'
+@Injectable({
+  providedIn: 'root',
 })
 export class GithubService {
-
   baseUrl: string = '';
- 
+
   constructor(private http: HttpClient) {
-    if(environment.production) {
-      this.baseUrl = 'https://us-central1-portfolio-remastered-api.cloudfunctions.net/portfolio_remastered';
+    if (environment.production) {
+      this.baseUrl =
+        'https://us-central1-portfolio-remastered-api.cloudfunctions.net/portfolio_remastered';
     } else {
-      this.baseUrl = 'http://localhost:5001/portfolio-remastered-api/us-central1/portfolio_remastered';
+      this.baseUrl =
+        'http://localhost:5001/portfolio-remastered-api/us-central1/portfolio_remastered';
     }
   }
-
 
   getGitHubRepos() {
     var url = this.baseUrl + '/github/';
